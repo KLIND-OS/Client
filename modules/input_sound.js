aacc1493e7aaf9d3093a00866585517ec71ce28d
JZITNIK-github <email@jzitnik.is-a.dev>
2024-05-14 22:00:29 +0200

    Minor changes

--- modules/input_sound.js
+++ modules/input_sound.js
@@ -1,43 +1,50 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 class InputSound {
-    static getAll(callback) {
-        exec(`pactl list sources | grep -ie "description:"|cut -d: -f2`, { name: "KLIND OS" }, function (error, stdout, stderr) {
-            if (error) {
-                throw new Error("Filed to get all devices!")
-            }
-            else {
-                var all = stdout.split("\n")
-                var ret = []
-                for (const string of all) {
-                    if (string.trim() == "") continue
-                    ret.push(string.trim())
-                }
-                callback(ret)
-            }
-        })
-    }
-    static set(description) {
-        exec(`pactl set-default-source $(pactl list sources|grep -C2 -F "Description: ${description}"|grep Name|cut -d: -f2|xargs)`, { name: "KLIND OS" }, function (error, stdout, stderr) {
-            if (error) {
-                console.log(stderr)
-                throw new Error("Filed to set device!")
-            }
-        })
-    }
-    static getDefault(callback) {
-        exec(`pactl list sources | grep -A1 "Name: $(pactl info | grep "Default Source" | awk '{print $3}')" | grep "Description" | awk -F": " "{print \$2}"`,
-        { name: "KLIND OS" },
-        function (error, stdout, stderr) {
-            if (error) {
-                throw new Error("Filed to set device!")
-            }
-            else {
-                callback(stdout.replace("Description: ", "").trim())
-            }
-        })
-    }
+  static getAll(callback) {
+    exec(
+      `pactl list sources | grep -ie "description:"|cut -d: -f2`,
+      { name: "KLIND OS" },
+      function (error, stdout, stderr) {
+        if (error) {
+          throw new Error("Filed to get all devices!");
+        } else {
+          var all = stdout.split("\n");
+          var ret = [];
+          for (const string of all) {
+            if (string.trim() == "") continue;
+            ret.push(string.trim());
+          }
+          callback(ret);
+        }
+      },
+    );
+  }
+  static set(description) {
+    exec(
+      `pactl set-default-source $(pactl list sources|grep -C2 -F "Description: ${description}"|grep Name|cut -d: -f2|xargs)`,
+      { name: "KLIND OS" },
+      function (error, stdout, stderr) {
+        if (error) {
+          console.log(stderr);
+          throw new Error("Filed to set device!");
+        }
+      },
+    );
+  }
+  static getDefault(callback) {
+    exec(
+      `pactl list sources | grep -A1 "Name: $(pactl info | grep "Default Source" | awk '{print $3}')" | grep "Description" | awk -F": " "{print \$2}"`,
+      { name: "KLIND OS" },
+      function (error, stdout, stderr) {
+        if (error) {
+          throw new Error("Filed to set device!");
+        } else {
+          callback(stdout.replace("Description: ", "").trim());
+        }
+      },
+    );
+  }
 }
 
-
-module.exports = InputSound
+module.exports = InputSound;

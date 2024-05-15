b925c0103a63cd3666c0c687518e64246afe7720
JZITNIK-github <email@jzitnik.is-a.dev>
2024-05-15 16:12:38 +0200

    Added consolelogs in update

--- modules/updates.js
+++ modules/updates.js
@@ -5,12 +5,14 @@ class Updates {
       callback("0%", "Aktualizování systému");
       exec(`pacman -Suy --noconfirm`, (error) => {
         if (error) {
+          console.log(error);
           callback("0%", "Error!");
           return;
         }
         callback("20%", "Aktualizace grafického rozhraní");
         exec(`rm -rf /root/klindos-server/data`, (error) => {
           if (error) {
+            console.log(error);
             callback("0%", "Error!");
             return;
           }
@@ -19,6 +21,7 @@ class Updates {
             `git clone --branch ${branch} --depth 1 https://github.com/KLIND-OS/Server /root/klindos-server/data`,
             (error) => {
               if (error) {
+                console.log(error);
                 callback("0%", "Error!");
                 return;
               }
@@ -27,12 +30,14 @@ class Updates {
                 `git clone --branch ${branch} --depth 1 https://github.com/KLIND-OS/Client /root/KLIND-OS-Client`,
                 (error) => {
                   if (error) {
+                    console.log(error);
                     callback("0%", "Error!");
                     return;
                   }
                   callback("50%", "Aktualizace klienta");
                   exec(`(cd /root/KLIND-OS-Client && npm install)`, (error) => {
                     if (error) {
+                      console.log(error);
                       callback("0%", "Error!");
                       return;
                     }
@@ -41,12 +46,14 @@ class Updates {
                       `(cd /root/KLIND-OS-Client && npm run build)`,
                       (error) => {
                         if (error) {
+                          console.log(error);
                           callback("0%", "Error!");
                           return;
                         }
                         callback("80%", "Aktualizace klienta");
                         exec(`rm -rf /root/client.AppImage`, (error) => {
                           if (error) {
+                            console.log(error);
                             callback("0%", "Error!");
                             return;
                           }
@@ -55,18 +62,21 @@ class Updates {
                             `cp /root/KLIND-OS-Client/dist/*.AppImage /root/client.AppImage`,
                             (error) => {
                               if (error) {
+                                console.log(error);
                                 callback("0%", "Error!");
                                 return;
                               }
                               callback("86%", "Aktualizace klienta");
                               exec(`rm -rf /root/KLIND-OS-Client`, (error) => {
                                 if (error) {
+                                  console.log(error);
                                   callback("0%", "Error!");
                                   return;
                                 }
                                 callback("90%", "Aktualizace window manager");
                                 exec(`xmonad --recompile`, (error) => {
                                   if (error) {
+                                    console.log(error);
                                     callback("0%", "Error!");
                                     return;
                                   }
@@ -78,6 +88,7 @@ class Updates {
                                     `(cd ~/packages && npm update)`,
                                     (error) => {
                                       if (error) {
+                                        console.log(error);
                                         callback("0%", "Error!");
                                         return;
                                       }

902967ea1dc97f3731219e880f4e875337ca3fd5
JZITNIK-github <email@jzitnik.dev>
2024-07-07 22:15:35 +0200

    translation: Remove status text from update
    Now, the UI will grab the status text from locales.

--- modules/updates.js
+++ modules/updates.js
@@ -13,42 +13,42 @@ class Updates {
 
     LowLevelApi.Branch.getSelected(async (branch) => {
       try {
-        callback("0%", "Aktualizování systému");
+        callback("0%");
         await execAsync(`pacman -Suy --noconfirm`);
-        callback("20%", "Aktualizace grafického rozhraní");
+        callback("20%");
 
         await execAsync(`rm -rf /root/klindos-server/data`);
-        callback("25%", "Aktualizace grafického rozhraní");
+        callback("25%");
 
         await execAsync(
           `git clone --branch ${branch} --depth 1 https://github.com/KLIND-OS/Server /root/klindos-server/data`,
         );
-        callback("45%", "Aktualizace klienta");
+        callback("45%");
 
         await execAsync(
           `git clone --branch ${branch} --depth 1 https://github.com/KLIND-OS/Client /root/KLIND-OS-Client`,
         );
-        callback("50%", "Aktualizace klienta");
+        callback("50%");
 
         await execAsync(`(cd /root/KLIND-OS-Client && npm install)`);
-        callback("60%", "Aktualizace klienta");
+        callback("60%");
 
         await execAsync(`(cd /root/KLIND-OS-Client && npm run build)`);
-        callback("80%", "Aktualizace klienta");
+        callback("80%");
 
         await execAsync(`rm -rf /root/client.AppImage`);
-        callback("83%", "Aktualizace klienta");
+        callback("83%");
 
         await execAsync(
           `cp /root/KLIND-OS-Client/dist/*.AppImage /root/client.AppImage`,
         );
-        callback("86%", "Aktualizace klienta");
+        callback("86%");
 
         await execAsync(`rm -rf /root/KLIND-OS-Client`);
-        callback("90%", "Aktualizace window manager");
+        callback("90%");
 
         await execAsync(`xmonad --recompile`);
-        callback("93%", "Aktualizace NodeJS knihoven");
+        callback("93%");
 
         await execAsync(`(cd ~/packages && npm update)`);
         callback(true);
